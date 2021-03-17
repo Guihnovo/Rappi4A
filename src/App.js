3a43be2f19a324622303272dbc9cd9e096395e2d
unknown <guilhermenovo22@gmail.com>
2021-03-17 15:32:09 -0300

    Restaurante

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Router } from './routes/Router';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './constants/theme';
+import GlobalState from './global/GlobalState';
+
+function App() {
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+
+      <GlobalState>
+        <Router />
+      </GlobalState>
+
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
